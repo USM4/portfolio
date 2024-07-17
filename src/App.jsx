b5656e2fdd0d4fc99aa6eea68648c6e2e951e382
Oussama Redoine <oredoine@e3r4p16.1337.ma>
2024-07-17 03:55:55 +0100

    changed post

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,54 @@
-import React, { useRef, useEffect, useState } from 'react';
-import reactLogo from "./assets/react.svg";
+import React, { useEffect } from 'react';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import "./App.css";
 import ReactTypingEffect from 'react-typing-effect';
-import * as THREE from 'three';
-
+import { Wireframe } from 'three/examples/jsm/Addons.js';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const mountRef = useRef(null);
-
   useEffect(() => {
+    const render = new THREE.WebGLRenderer();
+    render.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(render.domElement);
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    mountRef.current.appendChild(renderer.domElement);
-
-    const geometry = new THREE.BoxGeometry();
+    const camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    const controls = new OrbitControls(camera, render.domElement);
+    controls.update();
+    const axesHelper = new THREE.AxesHelper(5);
+    scene.add(axesHelper);
+    camera.position.set(-10,30,30);
+    const BoxGeometry = new THREE.BoxGeometry();
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-      const cube = new THREE.Mesh(geometry, material);
+    const cube = new THREE.Mesh(BoxGeometry, material);
+  
     scene.add(cube);
+    
+    const planeGeometry = new THREE.PlaneGeometry(30,30);
+    const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, side: THREE.DoubleSide});
+    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.rotation.x = -0.5 * Math.PI;
+    scene.add(plane);
 
-    camera.position.z = 5;
+    const gridHelper = new THREE.GridHelper(30);
+    scene.add(gridHelper);
 
-    const animate = function () {
-      requestAnimationFrame(animate);
+    const sphereGeometry = new THREE.SphereGeometry(4,50,50);
+    const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x0000FF , wireframe: true});
+    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    
+    scene.add(sphere);
+
+    function animate() {
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
-      renderer.render(scene, camera);
-    };
-
-    animate();
-
-    // Cleanup on component unmount
-    return () => {
-      mountRef.current.removeChild(renderer.domElement);
-    };
+      render.render(scene, camera); 
+    }
+    render.setAnimationLoop(animate);
   }, []);
 
   return (
@@ -61,7 +73,6 @@ function App() {
           />
         </h1>
       </div>
-      <div ref={mountRef} />
     </>
   );
 }
